9dbbc4774d0fb5ba50c545213a8ee2d2ed10b9f9
Thomas Clark <tjclark@wpi.edu>
2015-05-05 14:47:29 -0400

    Add the Connection class
    A connection is a rule stating that the value of one socket is always
    copied over to another socket without modification.

---
+++ src/main/java/edu/wpi/grip/core/Connection.java
@@ -0,0 +1,42 @@
+package edu.wpi.grip.core;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import edu.wpi.grip.core.events.SocketChangedEvent;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A connection is a rule that causes one socket to update to always the value of another socket.
+ */
+public class Connection<T> {
+    private Socket<? extends T> outputSocket;
+    private Socket<T> inputSocket;
+
+    /**
+     * @param eventBus     The Guava {@link EventBus} used by the application.
+     * @param outputSocket The socket to listen for changes in.
+     * @param inputSocket  A different socket to update when a change occurs in the first.
+     */
+    public Connection(EventBus eventBus, Socket<? extends T> outputSocket, Socket<T> inputSocket) {
+        this.outputSocket = outputSocket;
+        this.inputSocket = inputSocket;
+
+        checkNotNull(inputSocket);
+        checkNotNull(outputSocket);
+        checkNotNull(eventBus);
+
+        if (inputSocket == outputSocket) {
+            throw new IllegalArgumentException("inputSocket cannot be the same as outputSocket");
+        }
+
+        eventBus.register(this);
+    }
+
+    @Subscribe
+    public void onOutputChanged(SocketChangedEvent e) {
+        if (e.getSocket() == outputSocket) {
+            inputSocket.setValue(outputSocket.getValue());
+        }
+    }
+}
---
+++ src/test/java/edu/wpi/grip/core/ConnectionTest.java
@@ -0,0 +1,50 @@
+package edu.wpi.grip.core;
+
+import com.google.common.eventbus.EventBus;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConnectionTest {
+    EventBus eventBus = new EventBus();
+    SocketHint<Double> fooHint = new SocketHint<>("foo", Double.class);
+    SocketHint<Double> barHint = new SocketHint<>("bar", Double.class);
+    Double testValue = 12345.6789;
+
+    @Test
+    public void testInputSocketChanges() {
+        Socket<Double> foo = new Socket<>(eventBus, fooHint);
+        Socket<Double> bar = new Socket<>(eventBus, barHint);
+        Connection<Double> connection = new Connection<>(eventBus, foo, bar);
+
+        foo.setValue(testValue);
+        assertEquals(testValue, bar.getValue());
+
+        eventBus.unregister(connection);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testEventBusNotNull() {
+        Socket<Double> foo = new Socket<>(eventBus, fooHint);
+        Socket<Double> bar = new Socket<>(eventBus, barHint);
+        Connection<Double> connection = new Connection<>(null, foo, bar);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInputSocketNotEqualToOutputSocket() {
+        Socket<Double> foo = new Socket<>(eventBus, fooHint);
+        Connection<Double> connection = new Connection<>(eventBus, foo, foo);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testOutputSocketNotNull() {
+        Socket<Double> foo = new Socket<>(eventBus, fooHint);
+        Connection<Double> connection = new Connection<>(eventBus, null, foo);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testInputSocketNotNull() {
+        Socket<Double> foo = new Socket<>(eventBus, fooHint);
+        Connection<Double> connection = new Connection<>(eventBus, foo, null);
+    }
+}
