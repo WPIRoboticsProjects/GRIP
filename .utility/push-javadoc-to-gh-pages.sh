16cd198ab9f760dec27d1353ad43c4226500b2f2
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2015-08-27 19:26:02 -0400

    build(docs): Removes dev/null pipe till GH-Pages is working

--- .utility/push-javadoc-to-gh-pages.sh
+++ .utility/push-javadoc-to-gh-pages.sh
@@ -11,14 +11,14 @@ if [ "$TRAVIS_REPO_SLUG" == "WPIRoboticsProjects/GRIP" ] && [ "$TRAVIS_JDK_VERSI
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/WPIRoboticsProjects/grip gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/WPIRoboticsProjects/grip gh-pages #> /dev/null
 
   cd gh-pages
   git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages #> /dev/null
 
   echo -e "Published Javadoc to gh-pages.\n"
 
