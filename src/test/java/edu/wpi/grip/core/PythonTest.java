ab4bffaa212a07fef9d611ce6b0f6b947ef7cdf1
Tom Clark <the.thomas.j.clark@gmail.com>
2015-09-08 17:12:59 -0400

    PythonScriptOperation keeps track of the script's source code or URL
    This is necessary for serializing operations that are implemented in
    Python.  The naive automatic serialization would try to save the entire
    Jython internal structures, but all we really need to do is provide a
    way to get the Python source code.

--- src/test/java/edu/wpi/grip/core/PythonTest.java
+++ src/test/java/edu/wpi/grip/core/PythonTest.java
@@ -15,11 +15,6 @@ public class PythonTest {
     static final int a = 1234, b = 5678;
 
     EventBus eventBus = new EventBus();
-    Operation additionFromString = new PythonScriptOperation("import edu.wpi.grip.core as grip\nimport java.lang.Integer\n\ninputs = [\n    grip.SocketHint(\"a\", java.lang.Integer),\n    grip.SocketHint(\"b\", java.lang.Integer),\n]\n\noutputs = [\n    grip.SocketHint(\"c\", java.lang.Integer),\n]\n\ndef perform(a, b):\n    return a + b\n");
-    Operation addition = new PythonScriptOperation(PythonScriptOperation.class.getResourceAsStream("/edu/wpi/grip/scripts/addition.py"));
-    Operation additionSubtraction = new PythonScriptOperation(PythonScriptOperation.class.getResourceAsStream("/edu/wpi/grip/scripts/addition-subtraction.py"));
-    Operation additionWrongOutputCount = new PythonScriptOperation(PythonScriptOperation.class.getResourceAsStream("/edu/wpi/grip/scripts/addition-wrong-output-count.py"));
-    Operation additionWrongOutputType = new PythonScriptOperation(PythonScriptOperation.class.getResourceAsStream("/edu/wpi/grip/scripts/addition-wrong-output-type.py"));
 
     @BeforeClass
     public static void setupPythonProperties() {
@@ -30,6 +25,7 @@ public class PythonTest {
 
     @Test
     public void testPython() throws Exception {
+        Operation addition = new PythonScriptOperation(PythonTest.class.getResource("/edu/wpi/grip/scripts/addition.py"));
         Step step = new Step(eventBus, addition);
         Socket aSocket = step.getInputSockets()[0];
         Socket bSocket = step.getInputSockets()[1];
@@ -43,6 +39,7 @@ public class PythonTest {
 
     @Test
     public void testPythonAdditionFromString() throws Exception {
+        Operation additionFromString = new PythonScriptOperation("import edu.wpi.grip.core as grip\nimport java.lang.Integer\n\ninputs = [\n    grip.SocketHint(\"a\", java.lang.Integer),\n    grip.SocketHint(\"b\", java.lang.Integer),\n]\n\noutputs = [\n    grip.SocketHint(\"c\", java.lang.Integer),\n]\n\ndef perform(a, b):\n    return a + b\n");
         Step step = new Step(eventBus, additionFromString);
         Socket aSocket = step.getInputSockets()[0];
         Socket bSocket = step.getInputSockets()[1];
@@ -56,6 +53,7 @@ public class PythonTest {
 
     @Test
     public void testPythonMultipleOutputs() throws Exception {
+        Operation additionSubtraction = new PythonScriptOperation(PythonTest.class.getResource("/edu/wpi/grip/scripts/addition-subtraction.py"));
         Step step = new Step(eventBus, additionSubtraction);
         Socket aSocket = step.getInputSockets()[0];
         Socket bSocket = step.getInputSockets()[1];
@@ -71,6 +69,7 @@ public class PythonTest {
 
     @Test
     public void testPythonWrongOutputCount() throws Exception {
+        Operation additionWrongOutputCount = new PythonScriptOperation(PythonTest.class.getResource("/edu/wpi/grip/scripts/addition-wrong-output-count.py"));
         Step step = new Step(eventBus, additionWrongOutputCount);
         Socket aSocket = step.getInputSockets()[0];
         Socket bSocket = step.getInputSockets()[1];
@@ -84,6 +83,7 @@ public class PythonTest {
 
     @Test
     public void testPythonWrongOutputType() throws Exception {
+        Operation additionWrongOutputType = new PythonScriptOperation(PythonTest.class.getResource("/edu/wpi/grip/scripts/addition-wrong-output-type.py"));
         Step step = new Step(eventBus, additionWrongOutputType);
         Socket aSocket = step.getInputSockets()[0];
         Socket bSocket = step.getInputSockets()[1];
