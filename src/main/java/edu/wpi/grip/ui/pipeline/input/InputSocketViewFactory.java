742b06adca09e95844092ff02bac86bafb8cfe7e
Tom Clark <the.thomas.j.clark@gmail.com>
2015-10-25 15:35:26 -0400

    Refactor most InputSocketView types into subclasses
    Instead of having a huge switch statement with a bunch of checkArguments
    and other conditionals, it's much easier to maintain a different
    subclass for each type of input.  Right now, CheckboxInputSocketView,
    SliderInputSocketView, and SpinnerInputSocketView are implemented.
    
    With the logic of choosing what view to use encapsulated into a factory,
    it should be easier to add more advanced views without adding stuff to a
    large, multi-purpose InputSocketView class.

---
+++ src/main/java/edu/wpi/grip/ui/pipeline/input/InputSocketViewFactory.java
@@ -0,0 +1,55 @@
+package edu.wpi.grip.ui.pipeline.input;
+
+import com.google.common.eventbus.EventBus;
+import edu.wpi.grip.core.InputSocket;
+import edu.wpi.grip.core.SocketHint;
+
+/**
+ * Factory for constructing editable controls for input sockets
+ */
+public class InputSocketViewFactory {
+
+    /**
+     * Create an instance of {@link InputSocketView} appropriate for the given socket.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> InputSocketView<T> createInputSocketView(EventBus eventBus, InputSocket<T> socket) {
+        final SocketHint<T> socketHint = socket.getSocketHint();
+
+        switch (socketHint.getView()) {
+            case NONE:
+                return new InputSocketView<T>(eventBus, socket) {};
+
+            case SPINNER:
+                if (socketHint.getType().equals(Number.class)) {
+                    return (InputSocketView<T>) new NumberSpinnerInputSocketView(eventBus, (InputSocket<Number>) socket);
+                }
+
+                break;
+
+            case SLIDER:
+                if (socketHint.getType().equals(Number.class)) {
+                    return (InputSocketView<T>) new SliderInputSocketView(eventBus, (InputSocket<Number>) socket);
+                }
+
+                break;
+
+            case RANGE:
+                // TODO
+                break;
+
+            case SELECT:
+                // TODO
+                break;
+
+            case CHECKBOX:
+                if (socketHint.getType().equals(Boolean.class)) {
+                    return (InputSocketView<T>) new CheckboxInputSocketView(eventBus, (InputSocket<Boolean>) socket);
+                }
+
+                break;
+        }
+
+        throw new RuntimeException("Could not create view for socket: " + socket.toString());
+    }
+}
