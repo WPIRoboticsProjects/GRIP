e9235bb4687d3e7c4e1cdf7ac92e26fc55a35f5d
Paula Rudy <paularudy@gmail.com>
2015-11-29 14:57:04 -0500

    Fixed formatting.

--- src/main/java/edu/wpi/grip/ui/preview/PreviewsView.java
+++ src/main/java/edu/wpi/grip/ui/preview/PreviewsView.java
@@ -70,16 +70,16 @@ public class PreviewsView extends VBox {
 
             if (!this.previewedSockets.contains(socket)) {//If the socket is not already previewed...
 
-                if (socket.getStep().isPresent()){ //If this is a socket associated with a pipeline step (IE NOT a source)....
+                if (socket.getStep().isPresent()) { //If this is a socket associated with a pipeline step (IE NOT a source)....
 
                     Step socketStep = socket.getStep().get();//The pipeline step associated with the socket whose preview has changed
                     final StepView stepView = this.pipeline.findStepView(socketStep);//The gui object that displays the socketStep
                     int indexOfStep = this.pipeline.getSteps().indexOf(stepView); //The index of the step that has the socket in the pipeline
 
                     int numbOfSourcePreviews = 0;//This we will use to count how many *source* previews (not *step* previews) are currently displayed
-                    while((this.previewedSockets.size()>numbOfSourcePreviews) //While there are still previews to examine
+                    while ((this.previewedSockets.size() > numbOfSourcePreviews) //While there are still previews to examine
                             && ((this.previewedSockets.get(numbOfSourcePreviews).getSocketHint().getIdentifier().contains("Image")) //If this is a source (currently, sources can only have two types of output sockets, "Image" and "Frame Rate")...
-                               ||(this.previewedSockets.get(numbOfSourcePreviews).getSocketHint().getIdentifier().contains("Frame Rate"))))
+                            || (this.previewedSockets.get(numbOfSourcePreviews).getSocketHint().getIdentifier().contains("Frame Rate"))))
                         numbOfSourcePreviews++;
 
                     indexOfStep += numbOfSourcePreviews;//Add the number of source previews currently displayed to the index so that the source previews are always displayed first
@@ -93,9 +93,9 @@ public class PreviewsView extends VBox {
                         this.previewedSockets.add(indexFinal, socket);//...use this index to add it to the correct location in the list of previews open
                         this.previewBox.getChildren().add(indexFinal, SocketPreviewViewFactory.createPreviewView(this.eventBus, socket));//...and display it in the correct location in the list of previews open in the gui
                     }
-                }else{//This is a socket associated with a source and not a pipeline step...
-                    this.previewedSockets.add(0,socket);//...so add it to the beginning of the list
-                    this.previewBox.getChildren().add(0,SocketPreviewViewFactory.createPreviewView(this.eventBus, socket));//...and display it first in the preview gui window
+                } else {//This is a socket associated with a source and not a pipeline step...
+                    this.previewedSockets.add(0, socket);//...so add it to the beginning of the list
+                    this.previewBox.getChildren().add(0, SocketPreviewViewFactory.createPreviewView(this.eventBus, socket));//...and display it first in the preview gui window
                 }
             }
         } else {//The socket was already previewed, so the user must be requesting to not show this preview
