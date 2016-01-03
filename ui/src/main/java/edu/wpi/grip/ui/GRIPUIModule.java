1ec159593e95dd7490757274bdd3fd81e0b1d459
Thomas Clark <the.thomas.j.clark@gmail.com>
2016-01-02 23:31:31 -0500

    Revert "Feat: Remote Deployment and Running"

--- ui/src/main/java/edu/wpi/grip/ui/GRIPUIModule.java
+++ ui/src/main/java/edu/wpi/grip/ui/GRIPUIModule.java
@@ -10,8 +10,6 @@ import com.google.inject.spi.TypeListener;
 import edu.wpi.grip.core.Source;
 import edu.wpi.grip.ui.annotations.ParametrizedController;
 import edu.wpi.grip.ui.components.StartStoppableButton;
-import edu.wpi.grip.ui.deployment.FRCAdvancedDeploymentOptionsController;
-import edu.wpi.grip.ui.deployment.FRCDeploymentOptionsController;
 import edu.wpi.grip.ui.pipeline.OutputSocketController;
 import edu.wpi.grip.ui.pipeline.SocketHandleView;
 import edu.wpi.grip.ui.pipeline.StepController;
@@ -70,9 +68,6 @@ public class GRIPUIModule extends AbstractModule {
         install(new FactoryModuleBuilder().build(OperationController.Factory.class));
         install(new FactoryModuleBuilder().build(SocketHandleView.Factory.class));
         install(new FactoryModuleBuilder().build(OutputSocketController.Factory.class));
-        install(new FactoryModuleBuilder().build(DeployerController.Factory.class));
-        install(new FactoryModuleBuilder().build(FRCDeploymentOptionsController.Factory.class));
-        install(new FactoryModuleBuilder().build(FRCAdvancedDeploymentOptionsController.Factory.class));
         // End arbitrary controllers
 
         // InputSocketController Factories
