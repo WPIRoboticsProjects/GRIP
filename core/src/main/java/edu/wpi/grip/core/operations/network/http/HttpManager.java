136a50fff6df01272b67f4b15f4f7b5dd04d1520
Sam Carlberg <sam.carlberg@gmail.com>
2016-03-24 11:13:39 -0400

    Addressed first round of issues in PR

--- core/src/main/java/edu/wpi/grip/core/operations/network/http/HttpManager.java
+++ core/src/main/java/edu/wpi/grip/core/operations/network/http/HttpManager.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
+ * Manager for publishing data to the internal HTTP server.
  */
 public class HttpManager implements Manager, MapNetworkPublisherFactory {
 
