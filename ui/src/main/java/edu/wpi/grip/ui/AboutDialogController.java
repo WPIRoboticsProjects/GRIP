aa2f7c1d82b403b2077a58b361115379eb936295
Austin Shalit <austinshalit@gmail.com>
2016-05-21 17:48:58 -0700

    Add links to Github and ScreenSteps to the about dialog

--- ui/src/main/java/edu/wpi/grip/ui/AboutDialogController.java
+++ ui/src/main/java/edu/wpi/grip/ui/AboutDialogController.java
@@ -1,12 +1,28 @@
 package edu.wpi.grip.ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
+import javax.inject.Inject;
+
 public class AboutDialogController {
+
+    @Inject
+    private Main main;
 
     @FXML
     private Text versionNumberText;
+
+    @FXML
+    void githubButtonAction(ActionEvent event) {
+        main.getHostServices().showDocument("https://github.com/WPIRoboticsProjects/GRIP");
+    }
+
+    @FXML
+    void screenstepsButtonAction(ActionEvent event) {
+        main.getHostServices().showDocument("http://wpilib.screenstepslive.com/s/4485/m/50711");
+    }
 
     @FXML
     void initialize() {
