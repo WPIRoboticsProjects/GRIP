2296ba0a9525aee75518b0293a3a4dae95e4e146
Toby Macaluso <Toby.Macaluso@gmail.com>
2016-05-31 10:26:30 -0400

    Made outputs work for booleans and integers.

--- ui/src/main/java/edu/wpi/grip/ui/codeGeneration/java/TOutput.java
+++ ui/src/main/java/edu/wpi/grip/ui/codeGeneration/java/TOutput.java
@@ -8,9 +8,23 @@ public class TOutput extends TSocket {
 
   public TOutput(String type, int number){
     super(type);
+    if(super.type.equals("Number")){
+      super.type = "Integer";
+    }
     this.number = number;
     super.name = "output" +number;
   }
 
+  public String initialVal(){
+    if(super.type.equals("Integer")){
+      return "0";
+    }
+    else if(super.type.equals("Boolean")){
+      return "false";
+    }
+    else{
+      return "";
+    }
+  }
 
 }
