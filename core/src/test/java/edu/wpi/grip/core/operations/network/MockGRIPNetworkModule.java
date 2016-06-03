2c2c52d7d4b411fa1100817a77faae61f67d5330
Austin Shalit <austinshalit@gmail.com>
2016-06-03 11:09:30 -0700

    Add About window (#588)
    * Add About window
    
    * Use Application Modal instead of always on top
    
    * Add links to Github and ScreenSteps to the about dialog
    
    * Apply @JLLeitschuh patch
    
    * New button style
    
    * Fix open twice bug
    
    * Change Cursor to Hand while over links
    
    * Use CSS formating instead of Rectangle
    
    * Use utility style and autoclose
    
    * Fix Windows font bug
    
    * Fix tests so a mock network module is now injected
    
    * Updated to use Roboto fonts not loaded with CSS (#590)
    
    * Remove unused assert

---
+++ core/src/test/java/edu/wpi/grip/core/operations/network/MockGRIPNetworkModule.java
@@ -0,0 +1,21 @@
+package edu.wpi.grip.core.operations.network;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import edu.wpi.grip.core.operations.network.ros.MockROSManager;
+import edu.wpi.grip.core.operations.network.ros.ROSNetworkPublisherFactory;
+
+/**
+ * A mock of {@Link GRIPNetworkModule} for testing.
+ */
+public final class MockGRIPNetworkModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(MapNetworkPublisherFactory.class)
+                .annotatedWith(Names.named("ntManager"))
+                .to(MockMapNetworkPublisher.class);
+        bind(ROSNetworkPublisherFactory.class)
+                .annotatedWith(Names.named("rosManager"))
+                .to(MockROSManager.class);
+    }
+}
