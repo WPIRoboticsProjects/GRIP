ce8dc5695d8491b32c2cd790fa0d3791dec263a0
Patrick <pmurphy2@wpi.edu>
2016-06-20 15:13:34 -0400

    Beginning of testing java generation
    adds new switches for gradle:
    -Pgeneration to compile/run generation and non generation tests
    -Pgenonly to compile all tests but only run generation tests
    if neither is specified then generation tests will not be compiled
    and thus OpenCV is not required to compile tests other than generation

---
+++ ui/src/test/java/edu/wpi/grip/ui/codegeneration/tools/PipelineCreator.java
@@ -0,0 +1,53 @@
+package edu.wpi.grip.ui.codegeneration.tools;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.tools.JavaCompiler;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
+
+import org.opencv.core.Core;
+public class PipelineCreator {
+	static{
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	private static void compile(String fileName){
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+		try {
+			fileManager.setLocation(StandardLocation.CLASS_OUTPUT, Arrays.asList(PipelineGenerator.codeDir));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			fail("FileManager could not set output location");
+		}
+		compiler.getTask(null, fileManager, null, null, null, fileManager.getJavaFileObjects(PipelineGenerator.codeDir.toPath().resolve(fileName).toFile())).call();
+		try {
+			fileManager.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private static Class load(String fileName){
+		try {
+			return Class.forName(fileName.replace(".java", ""));
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			fail("Unable to load class");
+		}
+		return null;
+	}
+	public static Class makeClass(String fileName){
+		compile(fileName);
+		Class claz = load(fileName);
+		return claz;
+	}
+	public static void clean(String fileName){
+		PipelineGenerator.codeDir.toPath().resolve(fileName).toFile().deleteOnExit();
+		PipelineGenerator.codeDir.toPath().resolve(fileName.replace(".java", ".class")).toFile().deleteOnExit();
+	}
+}
---
+++ ui/src/test/java/edu/wpi/grip/ui/codegeneration/tools/PipelineInterfacer.java
@@ -0,0 +1,65 @@
+package edu.wpi.grip.ui.codegeneration.tools;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class PipelineInterfacer {
+	private Class pipeline;
+	private Object instance;
+	public PipelineInterfacer(String className){
+		pipeline = PipelineCreator.makeClass(className);
+		try {
+			instance = pipeline.getConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			fail("Failure to instansiate class");
+		}
+	}
+	public void setSource(int num, Object value){
+		try{
+			pipeline.getMethod("setsource"+num, value.getClass()).invoke(instance, value);
+		}catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			fail("setsource"+num +" is not valid for class "+value.getClass().getSimpleName());
+		}
+	}
+	public void process(){
+		try {
+			pipeline.getMethod("process").invoke(instance);
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException
+				| SecurityException e) {
+			e.printStackTrace();
+			fail("process method doesn't exist");
+		}
+	}
+	public Object getOutput(int num){
+		try{
+			return pipeline.getMethod("getoutput"+num).invoke(instance);
+		}catch(IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException
+				| SecurityException e){
+			e.printStackTrace();
+			fail("getoutput"+num +" method does not exist");
+			return null;
+		}
+	}
+	public void setSwitch(int num, boolean value){
+		try {
+			pipeline.getMethod("setSwitch"+num, boolean.class).invoke(instance, value);
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			fail("setSwitch"+num+"is not a valid method");
+		}
+	}
+	public void setValve(int num, boolean value){
+		try {
+			pipeline.getMethod("setValve"+num, boolean.class).invoke(instance, value);
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			fail("setValve"+num+"is not a valid method");
+		}
+	}
+}
