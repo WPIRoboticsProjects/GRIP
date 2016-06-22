a641d86e4b2d19972ba0326d49d88ab79c1009bb
Toby Macaluso <Toby.Macaluso@gmail.com>
2016-06-22 14:05:59 -0400

    Finished creating all of the Python Templates. More testing needed.

--- ui/src/main/java/edu/wpi/grip/ui/codegeneration/PythonTMethods.java
+++ ui/src/main/java/edu/wpi/grip/ui/codegeneration/PythonTMethods.java
@@ -6,26 +6,31 @@ import edu.wpi.grip.ui.codegeneration.data.TInput;
 import edu.wpi.grip.ui.codegeneration.data.TStep;
 
 
-public class PythonTMethods extends TemplateMethods  {
-  public PythonTMethods(){
+public class PythonTMethods extends TemplateMethods {
+  public PythonTMethods() {
     super();
   }
 
   @Override
   public String name(String name) {
-	  return "__".concat(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_UNDERSCORE, name.replaceAll("\\s", "")));
+    return "__".concat(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_UNDERSCORE, name.replaceAll("\\s", "")));
   }
 
   @Override
   public String callOp(TStep step) {
-	  StringBuilder method = new StringBuilder();
-	  method.append("self.").append(name(step.name())).append("(");
-	  for(TInput inp : step.getInputs()){
-		  method.append("self.__").append(inp.name()).
-		  append("s").append(step.num()).append(", ");
-	  }
-	  method.delete(method.length()-2, method.length());
-	  method.append(")");
+    StringBuilder method = new StringBuilder();
+    method.append("self.").append(name(step.name())).append("(");
+    for (TInput inp : step.getInputs()) {
+      method.append("self.__").append(inp.name()).
+          append("s").append(step.num()).append(", ");
+    }
+    if (step.name().equals("Threshold_Moving")) {
+      method.append("self.__lastImage");
+      method.append(step.num());
+      method.append(", ");
+    }
+    method.delete(method.length() - 2, method.length());
+    method.append(")");
     return method.toString();
   }
 }
