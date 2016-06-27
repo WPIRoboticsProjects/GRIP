fd991163d749d7edb5988a603bbdd4d6b30de8f9
Toby Macaluso <Toby.Macaluso@gmail.com>
2016-06-27 10:07:52 -0400

    Checkstyle fixes for testing.

--- core/src/test/java/edu/wpi/grip/core/operations/OperationsUtil.java
+++ core/src/test/java/edu/wpi/grip/core/operations/OperationsUtil.java
@@ -1,22 +1,28 @@
 package edu.wpi.grip.core.operations;
-import javax.inject.Inject;
+
+import edu.wpi.grip.core.OperationMetaData;
 
 import com.google.common.collect.ImmutableList;
+
+import javax.inject.Inject;
+
 import static org.junit.Assert.fail;
-import edu.wpi.grip.core.OperationMetaData;
+
 public class OperationsUtil {
-	@Inject
-	private static CVOperations cvOperations;
-	public static ImmutableList<OperationMetaData> operations(){
-		return cvOperations.operations();
-	}
-	public static OperationMetaData getMetaData(String opName){
-		for(OperationMetaData data : operations()){
-			if(data.getDescription().name().equalsIgnoreCase(opName)){
-				return data;
-			}
-		}
-		fail("Given operation name " + opName + " does not match any CV operation");
-		return null;//Never going to happen since line above throws error
-	}
+  @Inject
+  private static CVOperations cvOperations;
+
+  public static ImmutableList<OperationMetaData> operations() {
+    return cvOperations.operations();
+  }
+
+  public static OperationMetaData getMetaData(String opName) {
+    for (OperationMetaData data : operations()) {
+      if (data.getDescription().name().equalsIgnoreCase(opName)) {
+        return data;
+      }
+    }
+    fail("Given operation name " + opName + " does not match any CV operation");
+    return null; //Never going to happen since line above throws error
+  }
 }
