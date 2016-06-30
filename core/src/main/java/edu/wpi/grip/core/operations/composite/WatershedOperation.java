25a6567d1a1cee1a80c9051941c5dd1e8b567ac0
Sam Carlberg <sam.carlberg@gmail.com>
2016-06-30 14:51:33 -0400

    Should fix segfaulting

--- core/src/main/java/edu/wpi/grip/core/operations/composite/WatershedOperation.java
+++ core/src/main/java/edu/wpi/grip/core/operations/composite/WatershedOperation.java
@@ -11,25 +11,28 @@ import edu.wpi.grip.core.util.Icon;
 import com.google.common.collect.ImmutableList;
 
 import java.nio.ByteBuffer;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
-import static org.bytedeco.javacpp.opencv_core.CV_8U;
 import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
 import static org.bytedeco.javacpp.opencv_core.CV_8UC3;
 import static org.bytedeco.javacpp.opencv_core.LINE_8;
 import static org.bytedeco.javacpp.opencv_core.Mat;
 import static org.bytedeco.javacpp.opencv_core.MatVector;
 import static org.bytedeco.javacpp.opencv_core.Point;
+import static org.bytedeco.javacpp.opencv_core.Point2f;
 import static org.bytedeco.javacpp.opencv_core.Scalar;
+import static org.bytedeco.javacpp.opencv_core.bitwise_xor;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_CHAIN_APPROX_TC89_KCOS;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_FILLED;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_RETR_EXTERNAL;
 import static org.bytedeco.javacpp.opencv_imgproc.circle;
 import static org.bytedeco.javacpp.opencv_imgproc.drawContours;
 import static org.bytedeco.javacpp.opencv_imgproc.findContours;
+import static org.bytedeco.javacpp.opencv_imgproc.pointPolygonTest;
 import static org.bytedeco.javacpp.opencv_imgproc.watershed;
 
 /**
@@ -53,7 +56,6 @@ public class WatershedOperation implements Operation {
           .initialValueSupplier(ContoursReport::new)
           .build();
 
-  @SuppressWarnings("unchecked")
   private final SocketHint<ContoursReport> outputHint =
       new SocketHint.Builder<>(ContoursReport.class)
           .identifier("Features")
@@ -97,6 +99,14 @@ public class WatershedOperation implements Operation {
     final ContoursReport contourReport = contoursSocket.getValue().get();
     final MatVector contours = contourReport.getContours();
 
+    final int maxMarkers = 253;
+    if (contours.size() > maxMarkers) {
+      throw new IllegalArgumentException(
+          "A maximum of " + maxMarkers + " contours can be used as markers."
+              + " Filter contours before connecting them to this operation if this keeps happening."
+              + " The contours must also all be external; nested contours will not work");
+    }
+
     final Mat markers = new Mat(input.size(), CV_32SC1, new Scalar(0.0));
     final Mat output = new Mat(markers.size(), CV_8UC1, new Scalar(0.0));
 
@@ -107,51 +117,100 @@ public class WatershedOperation implements Operation {
       }
 
       // draw background marker a different color from the foreground markers
-      // TODO maybe make this configurable? There may be something in the corner
-      circle(markers, new Point(5, 5), 3, Scalar.WHITE, -1, LINE_8, 0);
+      Point backgroundLabel = fromPoint2f(findBackgroundMarker(markers, contours));
+      circle(markers, backgroundLabel, 1, Scalar.WHITE, -1, LINE_8, 0);
 
+      // Perform watershed
       watershed(input, markers);
+      markers.convertTo(output, CV_8UC1);
 
       // Create a new mat for each feature
-      markers.convertTo(output, CV_8UC1);
-      ByteBuffer buffer = output.createBuffer();
-      final int stride = buffer.capacity() / output.rows();
-      Map<Byte, Mat> segments = new LinkedHashMap<>(); // Map segment number to it's image
-      int bufIdx;
-      for (int y = 0; y < output.rows(); y++) {
-        for (int x = 0; x < output.cols(); x++) {
-          bufIdx = (stride * y) + (x * markers.channels());
-          byte val = buffer.get(bufIdx);
-          if (val == 0 || val == -1) {
-            // Background (0) or border (-1)
-            continue;
-          }
-          Mat m = segments.computeIfAbsent(val, k -> {
-            Mat segment = new Mat();
-            segment.create(markers.rows(), markers.cols(), CV_8U);
-            segment.<ByteBuffer>createBuffer().put(new byte[markers.rows() * markers.cols()]);
-            return segment;
-          });
-          ByteBuffer b = m.createBuffer();
-          b.put(bufIdx, Byte.MAX_VALUE);
+      final ByteBuffer buffer = output.createBuffer();
+      final Map<Byte, Mat> segments = new HashMap<>(); // Map segment number to its image
+      final Map<Byte, ByteBuffer> buffers = new HashMap<>(); // avoid creating tons of buffers
+      final int rows = output.rows();
+      final int cols = output.cols();
+      // Don't replace these functions with lambdas
+      final Function<Byte, Mat> newMat = k -> {
+        Mat segment = new Mat(rows, cols, CV_8UC1);
+        bitwise_xor(segment, segment, segment);
+        return segment;
+      };
+      final Function<Byte, ByteBuffer> newBuf = k -> segments.get(k).createBuffer();
+      for (int i = 0; i < rows * cols; i++) {
+        byte val = buffer.get(i);
+        if (val == 0 || val == -1) {
+          // Background (0) or border (-1)
+          continue;
         }
+        segments.computeIfAbsent(val, newMat);
+        buffers.computeIfAbsent(val, newBuf).put(i, Byte.MAX_VALUE);
+      }
+
+      // Should have one segmented image per contour
+      if (segments.size() != contours.size()) {
+        throw new IllegalStateException(
+            segments.size() + " != " + contours.size() + ". Are the contours nested?");
       }
 
       // Find the contours of the segmented features
       MatVector segmentContours = new MatVector(segments.size());
       segments.forEach((which, image) -> {
+        int index = which & 0xFF; // convert signed byte to unsigned
+        if (index < 1 || index > segmentContours.size()) {
+          throw new IndexOutOfBoundsException(
+              index + " < 1 or " + index + " > " + segmentContours.size());
+        }
         // This vector is guaranteed to contain exactly one contour
         MatVector theseContours = new MatVector();
         findContours(image, theseContours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_TC89_KCOS);
         Mat contour = theseContours.get(0);
-        segmentContours.put(which - 1, contour);
+        segmentContours.put(index - 1, contour);
+        image.release();
       });
 
-      outputSocket.setValue(new ContoursReport(segmentContours, markers.rows(), markers.cols()));
+      outputSocket.setValue(new ContoursReport(segmentContours, rows, cols));
     } finally {
       // make sure that the working mat is freed to avoid a memory leak
       markers.release();
     }
+  }
+
+  /**
+   * Finds the first available point to place a background marker for the watershed operation.
+   */
+  private Point2f findBackgroundMarker(Mat markers, MatVector contours) {
+    final int cols = markers.cols();
+    final int rows = markers.rows();
+    final int minDist = 5;
+    Point2f backgroundLabel = new Point2f();
+    boolean found = false;
+    // Don't place use a marker anywhere within 5 pixels of the edge of the image,
+    // or within 5 pixels of a contour
+    for (int x = minDist; x < cols - minDist && !found; x++) {
+      for (int y = minDist; y < rows - minDist && !found; y++) {
+        backgroundLabel.x(x);
+        backgroundLabel.y(y);
+        boolean isOpen = true;
+        for (int c = 0; c < contours.size(); c++) {
+          isOpen = pointPolygonTest(contours.get(c), backgroundLabel, true) <= -minDist;
+          if (!isOpen) {
+            // We know (x,y) is in a contour, don't need to check if it's in any others
+            break;
+          }
+        }
+        found = isOpen;
+      }
+    }
+    if (!found) {
+      // Should only happen if the image is clogged with contours
+      throw new IllegalStateException("Could not find a point for the background label");
+    }
+    return backgroundLabel;
+  }
+
+  private Point fromPoint2f(Point2f p) {
+    return new Point((int) p.x(), (int) p.y());
   }
 
 }
