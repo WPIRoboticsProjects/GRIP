013350b93bd205113d390f23a04cb8041e07aa87
Patrick <pmurphy2@wpi.edu>
2016-07-20 10:34:32 -0400

    Fixed incorrect brew command and hopefully the else error.

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -9,7 +9,7 @@ brew install python3
 brew upgrade python3
 pip3 install numpy
 pip3 install numpy --upgrade
-brew install opencv3 --with-python3 --with-java
+brew install homebrew/science/opencv3 --with-python3 --with-java
 export JNIDIR=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/
 export OPEN_CV_LIB=/usr/local/lib
 export OPEN_CV_JAR=/usr/local/share/OpenCV/java/opencv-310.jar
