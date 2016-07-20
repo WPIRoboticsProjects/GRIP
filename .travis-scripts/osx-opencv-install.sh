536ac3569890d3d47cea5a1c8dff471a50a2c542
Patrick <pmurphy2@wpi.edu>
2016-07-20 10:24:42 -0400

    Maybe brew works this time?

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -8,14 +8,8 @@ brew upgrade cmake
 brew install python3
 brew upgrade python3
 pip3 install numpy
-pip3 install numpy --ugrade
-git clone https://github.com/opencv/opencv.git
-cd opencv
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
+pip3 install numpy --upgrade
+brew install opencv3 --with-python3 --with-java
 export JNIDIR=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/
 export OPEN_CV_LIB=/usr/local/lib
 export OPEN_CV_JAR=/usr/local/share/OpenCV/java/opencv-310.jar
