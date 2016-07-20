7dcbad13bc181fb3cc4717b9968b1904c82d634e
Patrick <pmurphy2@wpi.edu>
2016-07-20 11:22:51 -0400

    Homebrew Opencv is broken and cannot install python3.
    Reverting to github and cmake.

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -7,9 +7,23 @@ brew install cmake
 brew upgrade cmake
 brew install python3
 brew upgrade python3
+brew linkapps python3
 pip3 install numpy
 pip3 install numpy --upgrade
-brew install homebrew/science/opencv3 --with-python3 --with-java
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+ -D PYTHON3_EXECUTABLE=$(which python3) \
+ -D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+ -D PYTHON3_LIBRARY=/usr/local/Cellar/python3/3.5.1/Frameworks/Python.framework/Versions/3.5/lib/libpython3.5.dylib \
+ -D PYTHON3_LIBRARIES=/usr/local/Cellar/python3/3.5.1/Frameworks/Python.framework/Versions/3.5/bin \
+ -D PYTHON3_INCLUDE_DIR=/usr/local/Cellar/python3/3.5.1/Frameworks/Python.framework/Versions/3.5/Headers \
+ -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") \
+ -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=ON \
+ -D BUILD_EXAMPLES=ON \
+ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules ..
 export JNIDIR=/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/
 export OPEN_CV_LIB=/usr/local/lib
 export OPEN_CV_JAR=/usr/local/share/OpenCV/java/opencv-310.jar
