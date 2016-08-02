WPIRoboticsProjects/GRIP

commit 2ab3774964d5be50ed8fd170ddaed9f08bf5a113

Adds save dirty checking event (#647)
* Appends "Edited" to the stage title on edit

core/src/main/java/edu/wpi/grip/core/serialization/Project.java
@@ -2,8 +2,10 @@ package edu.wpi.grip.core.serialization;
 
 import edu.wpi.grip.core.Pipeline;
 import edu.wpi.grip.core.PipelineRunner;
+import edu.wpi.grip.core.events.DirtiesSaveEvent;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.eventbus.Subscribe;
 import com.google.common.reflect.ClassPath;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
@@ -20,7 +22,6 @@ import java.io.StringReader;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -36,6 +37,7 @@ public class Project {
   @Inject
   private PipelineRunner pipelineRunner;
   private Optional<File> file = Optional.empty();
+  private boolean saveIsDirty = false;
 
   @Inject
   public void initialize(StepConverter stepConverter,
@@ -109,6 +111,7 @@ public class Project {
     this.pipeline.clear();
     this.xstream.fromXML(reader);
     pipelineRunner.startAsync();
+    saveIsDirty = false;
   }
 
   /**
@@ -124,5 +127,19 @@ public class Project {
 
   public void save(Writer writer) {
     this.xstream.toXML(this.pipeline, writer);
+    saveIsDirty = false;
+  }
+
+  public boolean isSaveDirty() {
+    return saveIsDirty;
+  }
+
+  @Subscribe
+  public void onDirtiesSaveEvent(DirtiesSaveEvent dirtySaveEvent) {
+    // Only update the flag the save isn't already dirty
+    // We don't need to be redundantly checking if the event dirties the save
+    if (!saveIsDirty && dirtySaveEvent.doesDirtySave()) {
+      saveIsDirty = true;
+    }
   }
 }

ui/src/main/java/edu/wpi/grip/ui/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.grip.ui;
 import edu.wpi.grip.core.GripCoreModule;
 import edu.wpi.grip.core.GripFileModule;
 import edu.wpi.grip.core.PipelineRunner;
+import edu.wpi.grip.core.events.DirtiesSaveEvent;
 import edu.wpi.grip.core.events.UnexpectedThrowableEvent;
 import edu.wpi.grip.core.http.GripServer;
 import edu.wpi.grip.core.http.HttpPipelineSwitcher;
@@ -31,6 +32,7 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.application.Preloader;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -43,6 +45,7 @@ public class Main extends Application {
 
   private final Object dialogLock = new Object();
   private static final Logger logger = Logger.getLogger(Main.class.getName());
+  private static final String MAIN_TITLE = "GRIP Computer Vision Engine";
 
   /**
    * JavaFX insists on creating the main application with its own reflection code, so we can't
@@ -60,6 +63,7 @@ public class Main extends Application {
   @Inject private HttpPipelineSwitcher pipelineSwitcher;
   private Parent root;
   private boolean headless;
+  private final SimpleBooleanProperty dirty = new SimpleBooleanProperty(false);
 
   public static void main(String[] args) {
     launch(args);
@@ -123,9 +127,17 @@ public class Main extends Application {
       cvOperations.addOperations();
       notifyPreloader(new Preloader.ProgressNotification(0.9));
 
+      dirty.addListener((observable, oldValue, newValue) -> {
+        if (newValue) {
+          stage.setTitle(MAIN_TITLE + " | Edited");
+        } else {
+          stage.setTitle(MAIN_TITLE);
+        }
+      });
+
       // If this isn't here this can cause a deadlock on windows. See issue #297
       stage.setOnCloseRequest(event -> SafeShutdown.exit(0, Platform::exit));
-      stage.setTitle("GRIP Computer Vision Engine");
+      stage.setTitle(MAIN_TITLE);
       stage.getIcons().add(new Image("/edu/wpi/grip/ui/icons/grip.png"));
       stage.setScene(new Scene(root));
       notifyPreloader(new Preloader.ProgressNotification(1.0));
@@ -137,6 +149,13 @@ public class Main extends Application {
 
   public void stop() {
     SafeShutdown.flagStopping();
+  }
+
+  @Subscribe
+  public void onDirtiesSaveEvent(DirtiesSaveEvent dirtySaveEvent) {
+    if (dirty.get() != dirtySaveEvent.doesDirtySave()) {
+      dirty.set(dirtySaveEvent.doesDirtySave());
+    }
   }
 
   @Subscribe

core/src/main/java/edu/wpi/grip/core/events/ConnectionAddedEvent.java
@@ -10,7 +10,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * An event that occurs when a new connection is added to the pipeline.  This is triggered by the
  * user adding a connection with the GUI.
  */
-public class ConnectionAddedEvent implements RunPipelineEvent {
+public class ConnectionAddedEvent implements RunPipelineEvent, DirtiesSaveEvent {
   private final Connection connection;
 
   /**

core/src/main/java/edu/wpi/grip/core/events/ConnectionRemovedEvent.java
@@ -10,7 +10,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * An event that occurs when a connection is removed from the pipeline.  This is triggered by the
  * user deleting a connection with the GUI.
  */
-public class ConnectionRemovedEvent {
+public class ConnectionRemovedEvent implements DirtiesSaveEvent {
   private final Connection connection;
 
   /**

core/src/main/java/edu/wpi/grip/core/events/DirtiesSaveEvent.java
@@ -1,0 +1,19 @@
+package edu.wpi.grip.core.events;
+
+/**
+ * An event that can potentially dirty the save file.
+ *
+ * <p>These events ensure that anything that changes causes the save file to be flagged as dirty and
+ * in need of being saved for the project to be deemed "clean" again.
+ */
+public interface DirtiesSaveEvent {
+
+  /**
+   * Some events may have more logic regarding whether they make the save dirty or not.
+   *
+   * @return True if this event should dirty the project save
+   */
+  default boolean doesDirtySave() {
+    return true;
+  }
+}

core/src/main/java/edu/wpi/grip/core/events/SocketChangedEvent.java
@@ -10,7 +10,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * An event that occurs when the value stored in a socket changes.  This can happen, for example, as
  * the result of an operation completing, or as a response to user input.
  */
-public class SocketChangedEvent implements RunPipelineEvent {
+public class SocketChangedEvent implements RunPipelineEvent, DirtiesSaveEvent {
   private final Socket socket;
 
   /**
@@ -31,19 +31,26 @@ public class SocketChangedEvent implements RunPipelineEvent {
     return socket.equals(this.socket);
   }
 
+  /**
+   * This event will only dirty the save if the InputSocket does not have connections.
+   * Thus the value can only have been changed by a UI component.
+   * If the socket has connections then the value change is triggered by another socket's change.
+   *
+   * @return True if this should dirty the save.
+   */
+  @Override
+  public boolean doesDirtySave() {
+    return pipelineShouldRun();
+  }
+
   @Override
   public boolean pipelineShouldRun() {
     /*
      * The pipeline should only flag an update when it is an input changing. A changed output
-      * doesn't mean
-     * the pipeline is dirty.
-     * If the socket is connected to another socket then then the input will only change
-     * because of the
-     * pipeline thread.
-     * In that case we don't want the pipeline to be releasing itself.
-     * If the connections are empty then the change must have come from the UI so we need to
-     * run the pipeline
-     * with the new values.
+     * doesn't mean the pipeline is dirty. If the socket is connected to another socket then then
+     * the input will only change because of the pipeline thread. In that case we don't want the
+     * pipeline to be releasing itself. If the connections are empty then the change must have come
+     * from the UI so we need to run the pipeline with the new values.
      */
     return socket.getDirection().equals(Socket.Direction.INPUT) && socket.getConnections()
         .isEmpty();

core/src/main/java/edu/wpi/grip/core/events/SocketPreviewChangedEvent.java
@@ -10,7 +10,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * An event that occurs when a {@link OutputSocket} is set to be either previewed or not previewed.
  * The GUI listens for these events so it knows which sockets to show previews for.
  */
-public class SocketPreviewChangedEvent {
+public class SocketPreviewChangedEvent implements DirtiesSaveEvent {
   private final OutputSocket socket;
 
   /**

core/src/main/java/edu/wpi/grip/core/events/SourceAddedEvent.java
@@ -13,7 +13,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
  *
  * @see Source
  */
-public class SourceAddedEvent {
+public class SourceAddedEvent implements DirtiesSaveEvent {
   private final Source source;
 
   /**

commit 7896dd8835a5e06f4dd318c19fb655fd21f55eca

Fix bug introduced in #647

core/src/main/java/edu/wpi/grip/core/serialization/Project.java
@@ -21,7 +21,10 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -37,7 +40,7 @@ public class Project {
   @Inject
   private PipelineRunner pipelineRunner;
   private Optional<File> file = Optional.empty();
-  private boolean saveIsDirty = false;
+  private final ObservableBoolean saveIsDirty = new ObservableBoolean();
 
   @Inject
   public void initialize(StepConverter stepConverter,
@@ -111,7 +114,7 @@ public class Project {
     this.pipeline.clear();
     this.xstream.fromXML(reader);
     pipelineRunner.startAsync();
-    saveIsDirty = false;
+    saveIsDirty.set(false);
   }
 
   /**
@@ -127,19 +130,42 @@ public class Project {
 
   public void save(Writer writer) {
     this.xstream.toXML(this.pipeline, writer);
-    saveIsDirty = false;
+    saveIsDirty.set(false);
   }
 
   public boolean isSaveDirty() {
-    return saveIsDirty;
+    return saveIsDirty.get();
+  }
+
+  public void addIsSaveDirtyConsumer(Consumer<Boolean> consumer) {
+    saveIsDirty.addConsumer(consumer);
   }
 
   @Subscribe
   public void onDirtiesSaveEvent(DirtiesSaveEvent dirtySaveEvent) {
     // Only update the flag the save isn't already dirty
     // We don't need to be redundantly checking if the event dirties the save
-    if (!saveIsDirty && dirtySaveEvent.doesDirtySave()) {
-      saveIsDirty = true;
+    if (!saveIsDirty.get() && dirtySaveEvent.doesDirtySave()) {
+      saveIsDirty.set(true);
+    }
+  }
+
+  private static final class ObservableBoolean {
+
+    private boolean b = false;
+    private final List<Consumer<Boolean>> consumers = new LinkedList<>();
+
+    public void set(boolean b) {
+      this.b = b;
+      consumers.parallelStream().forEach(c -> c.accept(b));
+    }
+
+    public boolean get() {
+      return b;
+    }
+
+    public void addConsumer(Consumer<Boolean> consumer) {
+      consumers.add(consumer);
     }
   }
 }

ui/src/main/java/edu/wpi/grip/ui/Main.java
@@ -3,7 +3,6 @@ package edu.wpi.grip.ui;
 import edu.wpi.grip.core.GripCoreModule;
 import edu.wpi.grip.core.GripFileModule;
 import edu.wpi.grip.core.PipelineRunner;
-import edu.wpi.grip.core.events.DirtiesSaveEvent;
 import edu.wpi.grip.core.events.UnexpectedThrowableEvent;
 import edu.wpi.grip.core.http.GripServer;
 import edu.wpi.grip.core.http.HttpPipelineSwitcher;
@@ -32,7 +31,6 @@ import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.application.Preloader;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -63,7 +61,6 @@ public class Main extends Application {
   @Inject private HttpPipelineSwitcher pipelineSwitcher;
   private Parent root;
   private boolean headless;
-  private final SimpleBooleanProperty dirty = new SimpleBooleanProperty(false);
 
   public static void main(String[] args) {
     launch(args);
@@ -127,11 +124,11 @@ public class Main extends Application {
       cvOperations.addOperations();
       notifyPreloader(new Preloader.ProgressNotification(0.9));
 
-      dirty.addListener((observable, oldValue, newValue) -> {
+      project.addIsSaveDirtyConsumer(newValue -> {
         if (newValue) {
-          stage.setTitle(MAIN_TITLE + " | Edited");
+          Platform.runLater(() -> stage.setTitle(MAIN_TITLE + " | Edited"));
         } else {
-          stage.setTitle(MAIN_TITLE);
+          Platform.runLater(() -> stage.setTitle(MAIN_TITLE));
         }
       });
 
@@ -149,13 +146,6 @@ public class Main extends Application {
 
   public void stop() {
     SafeShutdown.flagStopping();
-  }
-
-  @Subscribe
-  public void onDirtiesSaveEvent(DirtiesSaveEvent dirtySaveEvent) {
-    if (dirty.get() != dirtySaveEvent.doesDirtySave()) {
-      dirty.set(dirtySaveEvent.doesDirtySave());
-    }
   }
 
   @Subscribe