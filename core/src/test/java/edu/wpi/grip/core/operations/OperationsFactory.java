42e17fbf53a33caebc19ee922b6148c30d2cd555
Austin Shalit <austinshalit@gmail.com>
2016-08-22 07:50:28 -0400

    Update Gradle to v3.0

--- core/src/test/java/edu/wpi/grip/core/operations/OperationsFactory.java
+++ core/src/test/java/edu/wpi/grip/core/operations/OperationsFactory.java
@@ -51,19 +51,19 @@ public class OperationsFactory {
     }
 
     @Override
-    @SuppressWarnings("PMD.UncommentedEmptyMethod")
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
     public void publish(ROSMessagePublisher.Converter publish) {
 
     }
 
     @Override
-    @SuppressWarnings("PMD.UncommentedEmptyMethod")
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
     protected void publishNameChanged(Optional<String> oldName, String newName) {
 
     }
 
     @Override
-    @SuppressWarnings("PMD.UncommentedEmptyMethod")
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
     public void close() {
 
     }
