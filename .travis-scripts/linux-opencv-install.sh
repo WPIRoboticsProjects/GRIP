d9de34b6696bc5fa78872744e21d41fe3fdb5a0a
Sam Carlberg <sam.carlberg@gmail.com>
2016-09-26 18:23:02 -0400

    Use OpenCV artifacts on FRC maven for code gen testing. Enable code gen tests on Travis' OSX platform

--- .travis-scripts/linux-opencv-install.sh
+++ .travis-scripts/linux-opencv-install.sh
@@ -1,10 +1,8 @@
-pip3 install numpy
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D INSTALL_C_EXAMPLES=OFF \ 
--D BUILD_EXAMPLES=ON ..
-make
-make install
+#!/bin/bash
+
+#pip3 install --upgrade pip
+#pip3 install numpy
+#pip3 install opencv-python
+#mkdir -p $HOME/opencv/jni
+#sudo apt-get install libdc1394-22-dev
+#sudo ln /dev/null /dev/raw1394
