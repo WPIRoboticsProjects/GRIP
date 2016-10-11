bd8e8b3fdc1e772eb1bab27df59ea54ecf9f9993
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2016-10-10 23:20:09 -0400

    Add documentation to ApiConfig

--- web/src/main/java/edu/wpi/grip/web/swagger/api/APIConfig.java
+++ web/src/main/java/edu/wpi/grip/web/swagger/api/APIConfig.java
@@ -4,6 +4,9 @@ package edu.wpi.grip.web.swagger.api;
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
 
+/**
+ * Used to create the header to the <code>swagger.yaml/json</code> file when requested.
+ */
 @SwaggerDefinition(
     info = @Info(
         description = "GRIP as a Service",
