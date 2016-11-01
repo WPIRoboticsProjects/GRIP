5a010cd25e4c7420f63f795066cc4f10b94bae59
Sam Carlberg <sam.carlberg@gmail.com>
2016-11-01 11:24:50 -0400

    Revert accidental formatting change

--- core/src/test/java/edu/wpi/grip/core/sources/CameraSourceTest.java
+++ core/src/test/java/edu/wpi/grip/core/sources/CameraSourceTest.java
@@ -286,7 +286,7 @@ public class CameraSourceTest {
       for (int y = 0; y < frameIdx.rows(); y++) {
         for (int x = 0; x < frameIdx.cols(); x++) {
           for (int z = 0; z < frameIdx.channels(); z++) {
-            frameIdx.putDouble(new int[] {y, x, z}, y + x + z);
+            frameIdx.putDouble(new int[]{y, x, z}, y + x + z);
           }
         }
       }
