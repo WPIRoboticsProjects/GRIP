4c51b51a04784559485e75cb42ce58b0348466ee
Sam Carlberg <sam.carlberg@gmail.com>
2016-11-29 18:35:27 -0500

    Wait at least 1 second between gc calls. Improve Cleaner documentation

--- core/src/main/java/edu/wpi/grip/core/Cleaner.java
+++ core/src/main/java/edu/wpi/grip/core/Cleaner.java
@@ -20,21 +20,48 @@ import javax.annotation.Nullable;
  *
  * <p>JavaCV has a system property {@code org.bytedeco.javacpp.maxphysicalbytes} that it uses to
  * determine when to start deallocating native memory. However, this only results in calls to
- * {@code System.gc()}.</p>
+ * {@code System.gc()} and imposes a hard upper limit on native memory use, limiting large images
+ * or long pipelines. It's also not very portable: running from source needs it to be passed
+ * as a JVM argument with gradle, and it can't be adjusted based on the amount of memory on the
+ * system it's installed on. For us, manually running System.gc() periodically is a better solution.
+ * </p>
  */
 @Singleton
 public class Cleaner {
 
+  /**
+   * The minimum time delay before running System.gc(). This is in milliseconds. A gc call will
+   * never happen less than this amount of time after the previous call.
+   */
+  private static final long MIN_DELAY = 1000;
+
+  /**
+   * The maximum number of runs allowed before calling System.gc().
+   */
   private static final int MAX_RUNS_BEFORE_GC = 5;
+
+  /**
+   * The number of runs since the last gc call.
+   */
   private int runsSinceLastGc = 0;
+
+  /**
+   * Timestamp of the last gc call.
+   */
+  private long lastRun = 0;
 
   @Subscribe
   @SuppressFBWarnings(value = "DM_GC", justification = "GC is called infrequently")
   public void onRunFinished(@Nullable RunStoppedEvent e) {
     runsSinceLastGc++;
-    if (runsSinceLastGc == MAX_RUNS_BEFORE_GC) {
-      System.gc();
+    if (lastRun == 0) {
+      lastRun = System.nanoTime();
+    }
+    if (runsSinceLastGc >= MAX_RUNS_BEFORE_GC
+        && System.nanoTime() - lastRun >= MIN_DELAY * 1e6) {
       runsSinceLastGc = 0;
+      lastRun = System.nanoTime();
+      System.gc();
     }
   }
 
