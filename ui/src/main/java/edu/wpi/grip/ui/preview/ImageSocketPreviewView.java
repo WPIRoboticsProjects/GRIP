e75b1f6fdc3ce8b9859435e9a4d22627a4de520a
Sam Carlberg <sam.carlberg@gmail.com>
2016-12-20 13:32:31 -0500

    Dynamically resize image previews to fit the pane (#711)

--- ui/src/main/java/edu/wpi/grip/ui/preview/ImageSocketPreviewView.java
+++ ui/src/main/java/edu/wpi/grip/ui/preview/ImageSocketPreviewView.java
@@ -1,26 +1,18 @@
 package edu.wpi.grip.ui.preview;
 
-import edu.wpi.grip.core.events.RenderEvent;
 import edu.wpi.grip.core.sockets.OutputSocket;
 import edu.wpi.grip.ui.util.GripPlatform;
-import edu.wpi.grip.ui.util.ImageConverter;
 
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import static org.bytedeco.javacpp.opencv_core.Mat;
 
 /**
  * A <code>SocketPreviewView</code> that previews sockets containing OpenCV Mats.
  */
-public class ImageSocketPreviewView extends SocketPreviewView<Mat> {
+public class ImageSocketPreviewView extends ImageBasedPreviewView<Mat> {
 
   private final GripPlatform platform;
-  private final ImageConverter imageConverter;
-  private final ImageView imageView;
 
   /**
    * @param socket An output socket to preview.
@@ -28,27 +20,16 @@ public class ImageSocketPreviewView extends SocketPreviewView<Mat> {
   ImageSocketPreviewView(GripPlatform platform, OutputSocket<Mat> socket) {
     super(socket);
     this.platform = platform;
-    this.imageConverter = new ImageConverter();
-    this.imageView = new ImageView();
     this.setContent(imageView);
-
-    assert Platform.isFxApplicationThread() : "Must be in FX Thread to create this or you will be"
-        + " exposing constructor to another thread!";
-
-    convertImage();
   }
 
-  @Subscribe
-  public void onRender(RenderEvent event) {
-    convertImage();
-  }
-
-  private void convertImage() {
+  @Override
+  protected void convertImage() {
     synchronized (this) {
       this.getSocket().getValue().ifPresent(mat -> {
         platform.runAsSoonAsPossible(() -> {
-          final Image image = this.imageConverter.convert(mat);
-          this.imageView.setImage(image);
+          Image image = imageConverter.convert(mat, getImageHeight());
+          imageView.setImage(image);
         });
       });
     }
