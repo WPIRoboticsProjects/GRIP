ed3a7edcd2f969f88e8a5c42e090f30beeaf53ff
Sam Carlberg <sam.carlberg@gmail.com>
2016-12-27 16:37:27 -0500

    Limit camara connection retries to 50Hz

--- core/src/main/java/edu/wpi/grip/core/util/service/ServiceRestartPolicy.java
+++ core/src/main/java/edu/wpi/grip/core/util/service/ServiceRestartPolicy.java
@@ -7,12 +7,21 @@ package edu.wpi.grip.core.util.service;
 public interface ServiceRestartPolicy {
 
   /**
+   * A restart policy that has a service restart immediately.
+   */
+  ServiceRestartPolicy IMMEDIATE = () -> 0L;
+
+  /**
    * Policy might want to keep track of when the latest restarts have happened.
    */
   default void notifyRestart() {
-        /* no-op */
+    /* no-op */
   }
 
-  boolean shouldRestart();
+  /**
+   * When the service should restart, in nanoseconds from the point when the service failed. A value
+   * less than or equal to zero means an immediate restart.
+   */
+  long restartDelay();
 
 }
