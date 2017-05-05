c530c623ffbdd239c58f9da647c878da1b50724c
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-05 16:22:48 -0400

    Make the preloader have a fancy background (#843)
    * Make the preloader have a fancy background
    
    Remove the progress bar from the preloader as well
    
    * Make colors stand out a bit more
    
    * Change to dark color theme. Should probably depend on #832
    
    Small improvements to hex grid placement, no more weird boders

--- ui/preloader/src/main/java/edu/wpi/grip/preloader/GripPreloader.java
+++ ui/preloader/src/main/java/edu/wpi/grip/preloader/GripPreloader.java
@@ -1,17 +1,32 @@
 package edu.wpi.grip.preloader;
 
 import java.io.IOException;
+import java.util.Random;
+
+import javafx.animation.Animation;
+import javafx.animation.FillTransition;
+import javafx.application.Platform;
 import javafx.application.Preloader;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public final class GripPreloader extends Preloader {
 
-  private ProgressBar progressBar;
+  private static final Color primaryColor = Color.gray(0.075);
+  private static final Color secondaryColor = Color.gray(0.1);
+
+  // Animation timings
+  private static final double minTime = 1 / 3.0;
+  private static final double maxTime = 2 / 3.0;
+
+  private static final double HEXAGON_RADIUS = 12;
+
   private Stage preloaderStage;
 
   public static void main(String[] args) {
@@ -20,16 +35,41 @@ public final class GripPreloader extends Preloader {
 
   @Override
   public void start(Stage preloaderStage) throws IOException {
-    Parent root = FXMLLoader.load(GripPreloader.class.getResource("Preloader.fxml"));
-    Scene scene = new Scene(root);
+    final StackPane root = FXMLLoader.load(GripPreloader.class.getResource("Preloader.fxml"));
 
-    progressBar = (ProgressBar) root.getChildrenUnmodifiable().filtered(
-        p -> p instanceof ProgressBar).get(0);
+    // Animated hexagon grid background
+    // wrap in runLater so we can get the size of the scene
+    Platform.runLater(() -> {
+      Random random = new Random(System.currentTimeMillis() ^ (System.currentTimeMillis() >> 16));
+      HexagonGrid hexagonGrid = new HexagonGrid(
+          (int) (preloaderStage.getScene().getWidth() / HEXAGON_RADIUS),
+          (int) (preloaderStage.getScene().getHeight() / HEXAGON_RADIUS),
+          HEXAGON_RADIUS,
+          0);
+      // animate the hexagons
+      hexagonGrid.hexagons()
+          .stream()
+          .map(h -> new FillTransition(
+              Duration.seconds(
+                  clamp(random.nextGaussian() + 1, 0, 2) * (maxTime - minTime) + minTime),
+              h, primaryColor, secondaryColor))
+          .peek(t -> t.setCycleCount(Animation.INDEFINITE))
+          .peek(t -> t.setAutoReverse(true))
+          .forEach(t -> t.playFrom(Duration.seconds(random.nextDouble() * maxTime * 16)));
+      Pane backgroundContainer = new Pane(hexagonGrid);
+
+      // bring the hexagons to the top edge to avoid weird blank spots
+      backgroundContainer.setTranslateY(-HEXAGON_RADIUS);
+
+      root.getChildren().add(0, backgroundContainer);
+    });
+
+    Scene scene = new Scene(root);
 
     System.setProperty("prism.lcdtext", "false");
 
     if (getParameters().getRaw().contains("windowed")) {
-      preloaderStage.initStyle(StageStyle.UTILITY);
+      preloaderStage.initStyle(StageStyle.UNDECORATED);
     } else {
       preloaderStage.initStyle(StageStyle.TRANSPARENT);
     }
@@ -43,11 +83,14 @@ public final class GripPreloader extends Preloader {
 
   @Override
   public void handleApplicationNotification(PreloaderNotification pn) {
-    if (pn instanceof ProgressNotification) {
-      progressBar.setProgress(((ProgressNotification) pn).getProgress());
-    } else if (pn instanceof StateChangeNotification
+    if (pn instanceof StateChangeNotification
         && ((StateChangeNotification) pn).getType() == StateChangeNotification.Type.BEFORE_START) {
       preloaderStage.hide();
     }
   }
+
+  private static double clamp(double n, double min, double max) {
+    return (n < min) ? min : ((n > max) ? max : n);
+  }
+
 }
---
+++ ui/preloader/src/main/java/edu/wpi/grip/preloader/HexagonGrid.java
@@ -0,0 +1,63 @@
+package edu.wpi.grip.preloader;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import javafx.scene.Group;
+import javafx.scene.shape.Polygon;
+
+public class HexagonGrid extends Group {
+
+  private static final double ang30 = Math.toRadians(30);
+
+  /**
+   * Creates a new hexagon grid with the given number of rows and columns.
+   *
+   * @param cols    the number of columns in the grid
+   * @param rows    the number of rows in the grid
+   * @param radius  the radius of the hexagons in the grid
+   * @param padding the padding between each hexagon
+   */
+  public HexagonGrid(int cols, int rows, double radius, double padding) {
+    double xOffset = Math.cos(ang30) * (radius + padding);
+    double yOffset = Math.sin(ang30) * (radius + padding) * 3;
+    for (int row = 0; row < rows; row++) {
+      for (int col = 0; col < cols; col++) {
+        double x = xOffset * (col * 2 + row % 2);
+        double y = radius + yOffset * row;
+        Polygon hexagon = new Hexagon(radius);
+        hexagon.setRotate(90);
+        hexagon.setTranslateX(x);
+        hexagon.setTranslateY(y);
+        getChildren().add(hexagon);
+      }
+    }
+  }
+
+  /**
+   * Gets the hexagons in the grid. Do not apply any transforms to the hexagons; they are already
+   * in the correct locations and orientations.
+   */
+  public Collection<Polygon> hexagons() {
+    return getChildren().stream()
+        .filter(n -> n instanceof Hexagon)
+        .map(n -> (Hexagon) n)
+        .collect(Collectors.toList());
+  }
+
+  private static class Hexagon extends Polygon {
+
+    private static final double ROOT_THREE_OVER_2 = Math.sqrt(3) / 2;
+
+    public Hexagon(double radius) {
+      super(radius, 0,
+          radius / 2, ROOT_THREE_OVER_2 * radius,
+          -radius / 2, ROOT_THREE_OVER_2 * radius,
+          -radius, 0,
+          -radius / 2, -ROOT_THREE_OVER_2 * radius,
+          radius / 2, -ROOT_THREE_OVER_2 * radius);
+    }
+
+  }
+
+}
