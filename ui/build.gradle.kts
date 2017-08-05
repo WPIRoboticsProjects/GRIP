056bbaa32ddccbc4d909f8629cbe4f7cef15c2c1
Jonathan Leitschuh <Jlleitschuh@wpi.edu>
2017-08-05 19:21:23 -0400

    Rewrite generation test configuration

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,3 +1,23 @@
+
+import edu.wpi.first.wpilib.opencv.installer.Installer
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.testing.Test
+import org.gradle.api.tasks.testing.junit.JUnitOptions
+import org.gradle.script.lang.kotlin.*
+import org.gradle.util.GFileUtils
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://github.com/WPIRoboticsProjects/opencv-maven/raw/mvn-repo")
+        }
+    }
+    dependencies {
+        classpath(dependencyNotation = "edu.wpi.first.wpilib.opencv:opencv-installer:2.0.0")
+    }
+}
+
 plugins {
     java
     idea
@@ -27,16 +47,16 @@ dependencies {
     compile(project(path = ":core", configuration = "shadow"))
     compile(project(path = ":ui:preloader"))
     (ideProviderConfiguration.name)(project(path = ":core", configuration = "compile"))
-    compile (group= "org.controlsfx", name= "controlsfx", version= "8.40.11")
-    compile( group= "com.hierynomus", name= "sshj", version= "0.16.0")
-    compile( group= "org.apache.velocity", name= "velocity", version= "1.7")
+    compile(group = "org.controlsfx", name = "controlsfx", version = "8.40.11")
+    compile(group = "com.hierynomus", name = "sshj", version = "0.16.0")
+    compile(group = "org.apache.velocity", name = "velocity", version = "1.7")
     val coreOutput = coreProject.`java`.sourceSets["test"].output
-    testCompile( files(coreOutput.classesDirs))
-    testCompile( files(coreOutput.resourcesDir))
-    testCompile( group= "org.testfx", name= "testfx-core", version= "4.0.5-alpha")
-    testCompile( group= "org.testfx", name= "testfx-junit", version= "4.0.5-alpha")
-    testRuntime( group= "org.testfx", name= "openjfx-monocle", version= "1.8.0_20")
-    testCompile( group= "org.opencv", name= "opencv-java", version= "3.1.0")
+    testCompile(files(coreOutput.classesDirs))
+    testCompile(files(coreOutput.resourcesDir))
+    testCompile(group = "org.testfx", name = "testfx-core", version = "4.0.5-alpha")
+    testCompile(group = "org.testfx", name = "testfx-junit", version = "4.0.5-alpha")
+    testRuntime(group = "org.testfx", name = "openjfx-monocle", version = "1.8.0_20")
+    testCompile(group = "org.opencv", name = "opencv-java", version = "3.1.0")
 }
 
 evaluationDependsOn(":core")
@@ -123,17 +143,19 @@ fun setupTestSharedLib() {
         }
     }
     val testSharedLib = task<Copy>("testSharedLib") {
+        description = "Compiles the shared library used by c++ generation testing."
+        group = "build"
         dependsOn(testSharedLibStep2)
         into(testClassesDir) {
-            if(syst == "osx") {
+            if (syst == "osx") {
                 from("$testClassesDir/pipelib") {
                     include("libgenJNI.dylib")
                 }
-            } else if(syst == "linux") {
+            } else if (syst == "linux") {
                 from("$testClassesDir/pipelib") {
                     include("libgenJNI.so")
                 }
-            } else if(syst == "windows") {
+            } else if (syst == "windows") {
                 from("pipelib/Release") {
                     include("**")
                 }
@@ -155,6 +177,36 @@ fun setupTestSharedLib() {
     }
 }
 setupTestSharedLib()
+
+fun setupGenerationTests() {
+    fun getJniLocation() =
+            properties["jniLocation"]?.toString() ?:  Installer.getPlatform().defaultJniLocation()
+    val installOpenCVTask = task("installOpenCV") {
+        doLast {
+            val platform = Installer.getPlatform()
+            Installer.setOpenCvVersion("3.1.0")
+            val jniLocation = getJniLocation()
+            Installer.installJni(jniLocation)
+        }
+    }
+    tasks {
+        "test"(Test::class) {
+            dependsOn(installOpenCVTask)
+            val defaultLibPath = System.getProperty("java.library.path")
+            jvmArgs("-Djava.library.path=$defaultLibPath${System.getProperty("path.separator")}" +
+                            getJniLocation())
+            if(project.hasProperty("genonly")){
+                useJUnit {
+                    this as JUnitOptions
+                    includeCategories("edu.wpi.grip.ui.codegeneration.GenerationTesting")
+                }
+            }
+        }
+    }
+}
+if (project.hasProperty("generation") || project.hasProperty("genonly")) {
+    setupGenerationTests()
+}
 
 
 
