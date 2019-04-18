d4d5d5e9a4b606485f66909143b3d4e7a00b58ef
Sam Carlberg <sam.carlberg@gmail.com>
2019-04-18 17:45:05 -0400

    Manually set GH_TOKEN env var (#937)

--- .azure-scripts/push-javadoc-to-gh-pages.sh
+++ .azure-scripts/push-javadoc-to-gh-pages.sh
@@ -15,15 +15,15 @@ echo -e "Publishing javadoc...\n"
 cp -R build/docs/javadoc $HOME/javadoc-latest
 
 cd $HOME
-git config user.email "azuredevops@microsoft.com"
-git config user.name "azure-pipelines"
+git config --global user.email "azuredevops@microsoft.com"
+git config --global user.name "azure-pipelines"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/WPIRoboticsProjects/grip gh-pages #> /dev/null
 
 cd gh-pages
 git rm -rf ./javadoc
 cp -Rf $HOME/javadoc-latest ./javadoc
 git add -f .
-git commit -m "Lastest javadoc on successful Azure build ${BUILD_BUILD_NUMBER} auto-pushed to gh-pages"
+git commit -m "Lastest javadoc on successful Azure build ${BUILD_BUILDNUMBER} auto-pushed to gh-pages"
 git push -fq origin gh-pages #> /dev/null
 
 echo -e "Published Javadoc to gh-pages.\n"
