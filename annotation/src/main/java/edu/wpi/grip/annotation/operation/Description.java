71fac108e70039dad04b49315387c97c4d867a65
Sam Carlberg <sam.carlberg@gmail.com>
2019-04-18 18:28:41 -0400

    Cleanup javadoc, force UTF-8 encoding in Javadoc tasks
    Make `check` depend on javadoc task to enforce proper javadocs

--- annotation/src/main/java/edu/wpi/grip/annotation/operation/Description.java
+++ annotation/src/main/java/edu/wpi/grip/annotation/operation/Description.java
@@ -19,30 +19,40 @@ public @interface Description {
 
   /**
    * The name of the operation being described.
+   *
+   * @return the name of the operation
    */
   String name();
 
   /**
    * A brief summary of the operation. In-depth descriptions, usage guides, and examples
    * should be on the wiki, not here.
+   *
+   * @return a summary of the operation
    */
   String summary();
 
   /**
    * The category the operation belongs to. Defaults to
    * {@link OperationCategory#MISCELLANEOUS MISCELLANEOUS}.
+   *
+   * @return the category to which the operation belongs
    */
   OperationCategory category() default MISCELLANEOUS;
 
   /**
    * All known aliases of the operation. If the name of the operation changes, the previous name
    * should be here. Defaults to an empty array.
+   *
+   * @return known aliases
    */
   String[] aliases() default {};
 
   /**
    * The name of the icon to use to display the operation. If empty ({@code ""}), no icon will be
    * shown. The icon should be located in {@code /edu/wpi/grip/ui/icons/}.
+   *
+   * @return the name of the icon
    */
   String iconName() default "";
 
